ff419861babf8d21c83a3c1a1a3915965de3cfbf
cxf15364946594 <1261853718@qq.com>
2021-02-20 14:03:19 +0800

    增加商品分类和配置网关

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: "children",
+        label: "label"
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/pmscategory/list/tree"),
+        method: "get"
+      }).then(data => {
+        console.log("成功获取到菜单数据 .... " , data);
+      });
+    }
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）  组件创建完成就发送请求
+  created() {
+      this.getMenus();
+
+  },
+};
+</script>
+
+<style>
+</style>
