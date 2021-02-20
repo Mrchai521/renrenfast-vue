83984559a44932fbbc4ab13174cdc512a9d504a7
cxf15364946594 <1261853718@qq.com>
2021-02-20 17:46:20 +0800

    修改树形结构业务逻辑

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,15 @@
 <template>
-  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick" show-checkbox node-key="catId">
+     <span class="custom-tree-node" slot-scope="{ node, data }">
+      <!-- 显示节点的名称  -->
+      <span>{{ node.label }}</span>
+      <span>
+        <!-- 两个按钮    => 箭头函数 调用指定的方法 -->
+        <el-button v-if="node.level<=2" type="text" size="mini" @click="() => append(data)">展开</el-button>
+        <el-button v-if="node.childNodes.length==0" type="text" size="mini" @click="() => remove(node, data)">删除</el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
@@ -26,6 +36,12 @@ export default {
         console.log("成功获取到菜单数据 .... " , data.data);
         this.menus = data.data
       });
+    },
+    append(data) {
+      console.log("append " , data);
+    },
+    remove(node, data) {
+      console.log("remove" , node, data);
     }
   },
   //生命周期 - 创建完成（可以访问当前this实例）  组件创建完成就发送请求
